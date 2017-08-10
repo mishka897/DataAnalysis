7d262e4959d197c2591b559440c070d0f9b2090a
Mishka <30653179+mishka897@users.noreply.github.com>
2017-08-10 10:54:12 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Xamarin.Forms;
+
+namespace DataAnalysis
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+			InitializeComponent();
+
+            // Starting page 
+            MainPage = new DataAnalysisPage();
+
+/*
+
+            MainPage = new ContentPage()
+            {
+                //lbl and btn empty 
+
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,Children = 
+                    {
+                    btn1
+                    }
+                 }
+             };	
+
+
+            //btn clicked -> reader reads xml file and displays content on lbl1
+             btn1.Clicked += (sender, e) =>
+                        {
+                            btn1.Text = "You clicked me!!";
+                           // lbl1.Text = DataTemplates.reader.GetAttribute("Lieferdienst");
+                        };
+
+            // Second page displaying new content triggered by btn1
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        lbl1, btn1 
+                    }
+                }
+            };
+ */           
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DataAnalysisPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DataAnalysis
+{
+    public partial class DataAnalysisPage : ContentPage
+    {
+        public DataAnalysisPage()
+        {
+            InitializeComponent();
+		}
+
+        protected override void OnAppearing()
+        {
+            //lädt xml document als variable
+			var document = XDocument.Load("/Users/mishka/Projects/DataAnalysis/DataAnalysis/File.xml");
+            //erstellt zeile <Result> als variable
+			var row = document.Descendants("Result").FirstOrDefault();
+            //var element = document.Element("Lieferdienst");
+			if (row != null)
+			{
+                //erstellt Zeile <Lieferdienst> als variable
+				var ld = row.Element("Lieferdienst")?.Value;
+                LieferdienstLabel1.Text = "Lieferdienst: " + ld;
+                var s = row.Element("Service")?.Value;
+                ServiceLabel1.Text = "Service: " + s;
+				var j = row.Element("Jahr")?.Value;
+				var m = row.Element("Monat")?.Value;
+				var t = row.Element("Tag")?.Value;
+				DateLabel1.Text = "Datum: "+ t + "/" + m + "/" + j;
+				var a = row.Element("Anzahl")?.Value;
+				AnzahlLabel1.Text = "Anzahl: " + a;
+				var c = row.Element("Customer")?.Value;
+				CustomerLabel1.Text = "Customer: " + c;
+				var k = row.Element("Kanton")?.Value;
+				KantonLabel1.Text = "Kanton: " + k;
+    
+			}
+        }
+    }
+}
+
